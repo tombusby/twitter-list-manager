aea1046770266a831ef79bfbf1f1d63f77d62e8f
Tom Busby <tom@busby.ninja>
2020-08-22 23:22:11 +0200

    more stuff

--- test.py
+++ test.py
@@ -10,10 +10,22 @@ api = twitter.Api(**config['CREDENTIALS'])
 # owned_lists = api.GetLists()
 # pprint.pprint(owned_lists)
 
-user = api.GetUser(screen_name="_cryptoanarchy")
+# user = api.GetUser(screen_name="_cryptoanarchy")
+
 
 # for user in api.GetFriends():
-for user in [user]:
-    pprint.pprint(user)
-    print("Last Status At:", user.status.created_at)
-    print("============================")
+#     # for user in [user]:
+#     if user.status is not None and user.status.created_at_in_seconds < 1564617600:
+#         pprint.pprint(user)
+#         print("Last Status At:", user.status.created_at_in_seconds)
+#         print("============================")
+
+
+for screen_name in screen_names:
+    # pprint.pprint(user)
+    # pprint.pprint(user.description)
+    # print("====================")
+    print(screen_name)
+    api.DestroyFriendship(screen_name=screen_name)
+    api.CreateListsMember(screen_name=screen_name,
+                          list_id=1297270043580026880)
